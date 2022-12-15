f723455a62ef0c98676deee768969e9d76c7f352
lirabenjamin <lira.benjamin@gmail.com>
2022-12-15 16:48:36 -0500

    adding all R_files

---
+++ house features.R
@@ -0,0 +1,23 @@
+# waterfront
+# square feet
+# number of bedrooms
+# number of bathrooms
+# neighborhood score
+# crime rate
+# school scores
+# renovated when
+# garage for x cars
+
+library(tidyverse)
+tibble = tibble(
+  # waterfront
+  waterfront = sample(c(0,1), size = 100, replace = T)
+  # square feet
+  # number of bedrooms
+  # number of bathrooms
+  # neighborhood score
+  # crime rate
+  # school scores
+  # renovated when
+  # garage for x cars
+)
